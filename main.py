f91f4ee3d663848656bf6fe2e3c2d2230b8ef1cd
Олександр <Pioner322622@gmail.com>
2023-07-13 14:25:09 +0300

    first task

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 
+# task 1
+
+try:
+
+    name_line = input("Enter string: ")
+
+    letter = 0
+    digit = 0
+
+    for char in name_line:
+        if (char >= 'a' and char <= 'z') or (char >= 'A' and char <= 'Z'):
+            letter += 1
+        if char >= '0' and char <= '9':
+            digit += 1
+
+    print(f"Quantity of letter: {letter}")
+    print(f"Quantity of numbers: {digit}")
+
+except Exception as e:
+    print(f"Incorrect action{e}")
+finally:
+    print("End program\n")
